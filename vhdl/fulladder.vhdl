ea0136560b07d94637e85952db3e94653ac76a80
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2024-07-19 19:27:20 +0200

    Initial version

---
+++ vhdl/fulladder.vhdl
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity fulladder is
+port (
+    a, b, cin: in std_logic;
+    s, cout: out std_logic
+);
+end entity;
+
+architecture rtl of fulladder is
+begin
+    s <= a xor b xor cin;
+    cout <= (a and b) or (a and cin) or (a and b);
+end architecture;
