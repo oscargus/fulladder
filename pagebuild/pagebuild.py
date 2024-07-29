61343663e3e21228fdd904d6dd22cd703ce4c4f3
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2024-07-29 16:00:56 +0200

    Even better rendering, maybe correct

--- pagebuild/pagebuild.py
+++ pagebuild/pagebuild.py
@@ -22,10 +22,12 @@ with (html / 'index.html').open(mode='w') as f:
         path = Path(filename)
         url = escape(f"load_url=https://oscargus.github.io/fulladder/{filename}")
         maindir = path.parts[1]
+        # Filter out the parts, taken fron VUnit
         match = re.search(r"(.+)\.([^.]+)$", maindir)
         if match:
+            # Get rid of the trailing hash
             tmp_testname = match.group(2)
-            namematch = re.search(r"(.+)_[a-f0-9]+$")
+            namematch = re.search(r"(.+)_[a-f0-9]+$", tmp_testname)
             f.write(f"<li> Testbench: {match.group(1)}, test: {namematch.group(1)}. <a href={filename}>Download</a>, <a href=https://app.surfer-project.org/?{url}>Open in Surfer</a></li>\n")
         else:
             f.write(f"<li> {maindir}. <a href={filename}>Download</a>, <a href=https://app.surfer-project.org/?{url}>Open in Surfer</a></li>\n")
